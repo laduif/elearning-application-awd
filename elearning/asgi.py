e27c2a4981c59e2f3747ae108e98b523f790f18c
laduif <ladouife@gmail.com>
2026-09-14 01:03:03 +0100

    Fixed ASGI startup order

--- elearning/asgi.py
+++ elearning/asgi.py
@@ -1,27 +1,28 @@
-"""
-ASGI config for elearning project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
-"""
-
 import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elearning.settings')
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'elearning.settings'
+)
 
 django_asgi_application = get_asgi_application()
+
+
+import chat.routing
+
 
 application = ProtocolTypeRouter(
     {
         'http': django_asgi_application,
         'websocket': AuthMiddlewareStack(
-            URLRouter(chat.routing.websocket_urlpatterns )),
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        ),
     }
 )
